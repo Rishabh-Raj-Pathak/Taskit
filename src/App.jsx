63fba5ce34240601dbcaf8ea9b16ba3ece4e4e46
Rishabh Raj Pathak <rishabhrajpathak06@gmail.com>
2024-08-08 15:36:34 +0530

    initialized repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1 className='bg-black'>Bambu</h1>
+  )
+}
+
+export default App
